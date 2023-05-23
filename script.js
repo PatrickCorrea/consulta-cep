52e9418f5ed14d564c8f0633807d139d59210f51
Patrick Corrêa <patrickcorrea07@gmail.com>
2023-05-23 00:36:29 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function consultaEndereco() {
+    let cep = document.querySelector('#cep').value;
+
+    if (cep.length !== 8) {
+        alert('Cep invalido!');
+        return;
+    };
+
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url).then(function (response) {
+        response.json().then(mostrarEndereco);
+    });
+}
+
+function mostrarEndereco(dados) {
+    let resultado = document.querySelector('#resultado');
+
+    if (dados.erro) {
+        resultado.innerHTML = 'CEP não encontrado';
+    } else {
+        resultado.innerHTML = `<p>Endereço: ${dados.logradouro}</p>
+                <p>Bairro: ${dados.bairro}</p>
+                <p>Cidade: ${dados.localidade} - ${dados.uf}</p>`
+    };
+
+    document.querySelector('#cep').value = ""
+}  
